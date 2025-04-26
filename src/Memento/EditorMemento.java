7326244a189bdaa427c53c3b2451dbbcf3678a50
Leonardo27117 <zS23014080@estudiantes.uv.mx>
2025-04-25 23:39:43 -0600

    Patron memento

---
+++ src/Memento/EditorMemento.java
@@ -0,0 +1,13 @@
+package Memento;
+
+class EditorMemento {
+    private final String content;
+
+    public EditorMemento(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/Memento/History.java
@@ -0,0 +1,16 @@
+package Memento;
+import java.util.Stack;
+
+class History {
+    private final Stack<EditorMemento> history = new Stack<>();
+
+    public void saveState(TextEditor editor) {
+        history.push(editor.save());
+    }
+
+    public void undo(TextEditor editor) {
+        if (!history.isEmpty()) {
+            editor.restore(history.pop());
+        }
+    }
+}
